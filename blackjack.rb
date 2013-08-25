5f681a5f968f61fc406a226e70dada9ae3d57e44
Stephanie Viccari <Stephanieviccari.fl@gmail.com>
2013-08-25 16:03:49 -0400

    Added dealer's turn and calculates dealer score.

--- blackjack.rb
+++ blackjack.rb
@@ -42,9 +42,37 @@ end
 puts "Player score is: #{@total}"
 end
 
+def calculate_dealer_score
+  @dealer_total = 0
+  @dealer_hand.each do |card|
+    value = card.chop
+  if ["J", "Q", "K"].include?(value)
+     @dealer_total += 10
+  elsif value == "A" 
+    @dealer_total +=1 
+  else
+    @dealer_total += value.to_i
+  end
+end
+puts "Dealer score is: #{@dealer_total}"
+end
+
+def dealer_turn
+  while @dealer_total < 17
+    deal_dealer
+    puts calculate_dealer_score
+  end
+      if @dealer_total > 21
+        puts "Dealer busted. You win!"
+      else
+        puts "Dealer is standing."
+      end
+end 
+
+
 def validate_answer
   @input = gets.chomp
-  while !/[hs]/.match (@input.downcase)
+  while !/[hs]/.match(@input.downcase)
     puts "Please enter H or S"
     @input=gets.chomp
   end
@@ -52,21 +80,22 @@ def validate_answer
 end
 
 def hit_or_stand
-  while @input.match ("h")
+  while @input.match("h")
     deal_player
     puts calculate_player_score
       if @total > 21
         puts "You loose!!!!!!! HOUSE WINS!"
         break
       elsif
-        print "Do you want to hit or stand (H/S):"
+        puts "Do you want to hit or stand (H/S):"
         validate_answer
       end
-  until @input.match ("s")
+  until @input.match("s")
     break
-  end 
   end
-end
+  end
+    puts "Okay. Dealer's turn."
+end 
 
 
 
@@ -85,117 +114,24 @@ puts calculate_player_score
 print "Do you want to hit or stand (H/S):"
 
 validate_answer
-hit_or_stand
-
-
-#deal_dealer
-# deal_dealer
-# calculater_dealer_score
-
-
-
-
-
-
-
-
-
-# def hit_or_stand 
-#   input = gets.chomp
-#     while input.downcase == "h"
-#       deal_player
-#       print calculate_player_score
-
-#   else 
-#     reprint score
-#     puts "OK standing.."
-#   end
-# end
-
-#   deal dealer
-
+puts hit_or_stand
 
 
 deal_dealer
+deal_dealer
+calculate_dealer_score
+puts dealer_turn
 
-#Jack, Queen, King = 10pts
+
+
 #Ace = 1 or 11 points
-#player is dealt first
-#player can continue playing until they hit 21 or higher
-#player can choose to stand
 #dealer begins playing after the player chooses to stand
-#dealer can stand
 #dealer can play unless they have 17...21
-
-#if player goes over 21, dealer wins
 #if dealer goes over 21, player wins
 #person with a sum closest to 21 wins
 #if player sum == dealer sum, no one wins
 
 
-# *****Method********
-# def display_score
-# 	puts calculate_score
-# end
-# **********************
-
-# *****Method********
-# def hit_or_stand(calculate_score)
-# 	if calculate_score < 21 
-#     puts "Do you want to Hit or Stand (H/S)?:"
-# end
-# **********************
-
-# input = gets.chomp
-
-# *****Method********
-# def validate_input
-#  validate if user entered H/S or h/s?
-#  if not, prompt user to press H or S. 
-# end
-# **********************
-
-
-# *****Method********
-# def hit
-# if input == "h" || "H"
-# 	call Method Deal
-# 	call Method calculate_score
-# 	call display_score
-# **********************
-
-# *****Method********
-# def store_player_hand
-# 	player_hand = []
-# 	player_hand << deck.pop
-# end
-# **********************
-
-
-# *****Method********
-# def stand
-# 	if input == "s" || "S"
-# 	puts "Okay. Standing."
-# end
-# **********************
-
-# 
-# swith to dealer and add card(s) until card value >= 17
-# call deal method
-# call store_dealer_hand method
-# call deal method
-# call store_dealer_hand method
-# call calculate_score method
-# call stand method 
-# 
-# 
-# # *****Method********
-# # def store_dealer_hand
-# #   dealer_hand = []
-# #   dealer_hand << deck.pop
-# # end
-# # *********************
-# 
 # *****Method******** ???Boolean Value? 
 # def stand(dealer_score)
 #   if dealer calculate score >= 17
@@ -204,36 +140,15 @@ deal_dealer
 #     puts "Dealer will hit."
 # end
 # **********************
-# 
-# 
-# # *****Method********
-# def bust(score)
-# 	is score > 21
-# 	puts "Sorry! You busted."
-# end
-# **********************
 
 
 # *****Method********
 # def winner(player_score,dealer_score) 
-# 	if player calculated score > dealer calculated score
+#   if player calculated score > dealer calculated score
 #     puts "You win!"
 # # elsif player calculated socre == dealer calculated score
-	  # puts "Its a tie!"
-	#else
-# 	  puts "Dealer wins!"
+    # puts "Its a tie!"
+  #else
+#     puts "Dealer wins!"
 # end
 # **********************
-
-
-
-
-
-# *****Method********
-# def dealer_hit
-# 	receive a new card until unless score >= 17
-# 	if over 21
-# 		puts "Bust! You win!"
-# end
-# **********************
-#
